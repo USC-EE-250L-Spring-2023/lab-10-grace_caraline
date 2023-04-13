888f5f1817ce25b4a2a6105eed6da677e07b1ded
20gtrue <79179208+20gtrue@users.noreply.github.com>
2023-04-13 16:33:17 -0700

    Update server.py

--- server.py
+++ server.py
@@ -11,3 +11,31 @@ def index():
 
 # TODO: Create a flask app with two routes, one for each function.
 # The route should get the data from the request, call the function, and return the result.
+
+@app.route('/process1', methods=['POST'])
+def process1_remote():
+    """
+    Summary: runs process 1 on pc
+    
+    Returns:
+        str: list of prime numbers
+    """
+    data1 = request.get_json(['data'])
+    data1_return = process1(data1)
+    return data1_return
+
+    
+    @app.route('/process2', methods=['POST'])
+def process2_remote():
+    """
+    Summary: runs process 2 on pc
+    
+    Returns:
+        str: list of prime numbers
+    """
+    data2 = request.get_json(['data'])
+    data2_return = process2(data2)
+    return data2_return
+
+if __name__ == "__main__":
+    app.run()
